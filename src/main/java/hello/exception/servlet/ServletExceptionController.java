8b3abd0bc00bea71e8eb41e95e4d163a6694b202
HyeontaeLim <dlagusxo3827@naver.com>
2024-05-29 18:35:29 +0900

    first commit

---
+++ src/main/java/hello/exception/servlet/ServletExceptionController.java
@@ -0,0 +1,27 @@
+package hello.exception.servlet;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+@Controller
+public class ServletExceptionController {
+
+    @GetMapping("/error-ex")
+    public void errorEx() {
+        throw new RuntimeException();
+    }
+
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse response) throws IOException {
+        response.sendError(404, "404 오류!");
+    }
+
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse response) throws IOException {
+        response.sendError(500);
+    }
+}
